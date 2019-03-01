d197b23287bc9f81d80c770ddf8bdc2017bc714c
bzanetis <britt.zanetis19@gmail.com>
2019-02-28 22:26:55 -0600

    Installed Faker and Knex/Knex config file. Setting up enviro for seeding & data generation.

--- database/index.js
+++ database/index.js
@@ -1,30 +1,11 @@
-const Sequelize = require ('sequelize');
+var config = require("../knexfile"); 
+var env = "development"; 
+var knex = require("knex")(config[env]); 
 
-const db = new Sequelize('reviews', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
 
-const Reviews = db.define('reviews', {
-  id: {
-    type: Sequelize.INTEGER(11),
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  rating: Sequelize.INTEGER,
-  recommend: Sequelize.BOOLEAN,
-  size: Sequelize.INTEGER,
-  width: Sequelize.INTEGER,
-  comfort: Sequelize.INTEGER,
-  quality: Sequelize.INTEGER,
-  opinion: Sequelize.STRING(90),
-  review: Sequelize.STRING(16383),
-  nickname: Sequelize.STRING(20),
-  email: Sequelize.STRING(320)
-})
 
-//;
 
-module.exports.db = db
-module.exports.Reviews = Reviews
+
+
+
+module.exports = knex;
---
+++ knexfile.js
@@ -0,0 +1,56 @@
+module.exports = { 
+  development: { 
+    client: "postgresql", 
+    connection: { 
+      host: '127.0.0.1', 
+      user: 'postgres',
+      password: '', 
+      port: '5432', 
+      database: 'reviews_db' //my pg database
+    },
+    migrations: { 
+      directory: __dirname + "/database/migrations" 
+    }, 
+    seeds: { 
+      directory: __dirname + "/database/seed" 
+    } 
+  } 
+
+
+  // staging: { 
+  //   client: "postgresql", 
+  //   connection: { 
+  //     host: process.env.RDS_HOSTNAME, 
+  //     user: process.env.RDS_USERNAME, 
+  //     password: process.env.RDS_PASSWORD, 
+  //     port: process.env.RDS_PORT, 
+  //     database: process.env.RDS_DB_NAME 
+  //   }, 
+  //   pool: { 
+  //     min: 0, 
+  //     max: 10 
+  //   }, 
+  //   migrations: { 
+  //     tableName: "knex_migrations" 
+  //   } 
+  // }, 
+
+
+// production: { 
+//   client: "postgresql", 
+//   connection: { 
+//     host: process.env.RDS_HOSTNAME, 
+//     user: process.env.RDS_USERNAME, 
+//     password: process.env.RDS_PASSWORD, 
+//     port: process.env.RDS_PORT, 
+//     database: process.env.RDS_DB_NAME 
+//   }, 
+//   pool: { 
+//     min: 0, 
+//     max: 10 
+//   }, 
+//   migrations: { 
+//     directory: __dirname + "/database/seeds" 
+//   } 
+//} 
+};
