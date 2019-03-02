b2c6673ed918c46e80de6b048b04fd6a511e3994
bzanetis <britt.zanetis19@gmail.com>
2019-03-01 23:13:16 -0600

    Completed schema/seeding script for reviews, successful test of 10 reviews added

--- database/seed.js
+++ database/seed.js
@@ -3,6 +3,47 @@ var env = "development"; 
 var knex = require("knex")(config[env]); 
 const faker = require("faker"); 
 
+/*
+Reviews:
+id: assigned by pg
+nickname: chars
+review: chars
+rating: int w/range 0-5
+recommend: boolean
+createdAt: date
+index: int, rand w/range 0 - 5mil
+
+nickname: faker.internet.userName()
+review: faker.lorem.sentences()
+rating: faker.random.number({
+  min: 0,
+  max: 5
+})
+recommend: faker.random.boolean()   //or faker.random.arrayElement(["true", "false"])
+createdAt: faker.date.past()
+index: faker.number.between({
+  min: 0,
+  max: 100,000
+})
+*/
+
+const createFakeReview = () => {
+  return {
+    nickname: faker.internet.userName(),
+    review: faker.lorem.sentences(),
+    rating: faker.random.number({
+      min: 0,
+      max: 5
+    }),
+    recommend: faker.random.boolean(),
+    createdAt: faker.date.past(),
+    index: faker.random.number({
+      min: 0,
+      max: 100000
+    })
+  }
+}
+
 
 const createFakeProduct = () => {
   return {
@@ -12,18 +53,17 @@ const createFakeProduct = () => {
 
 
 
-
-let seed = function(){   
-  const fakeProducts = [];
-  const total = 5;
+let seedReviews = function(){   
+  const fakeReviews = [];
+  const total = 10;
     for (var i = 0; i < total; i++) {
-      fakeProducts.push(createFakeProduct());
+      fakeReviews.push(createFakeReview());
     }
 
-    return knex("products").insert(fakeProducts) //invoke faker func until i = 50k
+    return knex("reviews").insert(fakeReviews) //invoke faker func until i = 50k
     .then(function () { 
 
-  console.log('Seeding 5 records!')
+  console.log('Seeding 5 review records!')
   }) 
 } 
-seed(); 
+seedReviews(); 
